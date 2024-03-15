d2cb626f3cdbed6aae46530a5c39fee80bd3a36e
OlivierRustige <o.rustige@st.hanze.nl>
2024-03-15 12:23:01 +0100

    Dashboard, dashboard view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
-dashboard_blueprint = Blueprint('dashboard', __name__ , template_folder='templates')
+dashboard_blueprint = Blueprint('dashboard', __name__, template_folder='templates')
+
 
 @dashboard_blueprint.route('/')
+@login_required
 def dashboard():
     return render_template('dashboard.html')
